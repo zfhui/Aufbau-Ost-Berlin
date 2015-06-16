e1425feb208719266719b89dc6d460041827d482
Zhuo-Fei Hui <zhfhui+github@gmail.com>
2015-06-16 13:31:34 +0200

    Merge with master.

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,10 +1,9 @@
 class Building < ActiveRecord::Base
-<<<<<<< HEAD
+
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       Building.create! row.to_hash
     end
   end
-=======
->>>>>>> b99882b42fcfa3b0869df21e387ab551b50f5b5c
+
 end
