8ba15e134980c20591cda1efcf486fe26f4905ad
Zhuo-Fei Hui <zhfhui+github@gmail.com>
2015-06-16 13:26:59 +0200

    Merge with master.

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,9 @@ Rails.application.routes.draw do
     collection { post :import}
   end
 
-<<<<<<< HEAD
   resources :buildings do
     collection { post :import}
   end
-=======
-  resources :buildings
->>>>>>> b99882b42fcfa3b0869df21e387ab551b50f5b5c
 
   devise_for :users
 
