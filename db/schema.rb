1e1c60d3e7c603b0cfcec4171f27e25351a2ea14
Zhuo-Fei Hui <zhfhui+github@gmail.com>
2015-06-16 13:22:19 +0200

    Removes features from photos.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616092448) do
+ActiveRecord::Schema.define(version: 20150616111921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,13 +46,10 @@ ActiveRecord::Schema.define(version: 20150616092448) do
     t.string   "architect"
     t.string   "building_name"
     t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
     t.text     "content"
     t.string   "ownership"
     t.string   "creditline"
     t.float    "flicker_id"
-    t.text     "comment"
   end
 
   create_table "users", force: :cascade do |t|
