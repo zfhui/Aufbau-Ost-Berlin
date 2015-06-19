18bb9fda9096681ad7f8fbc984096a5114b8dc78
Zhuo-Fei Hui <zhfhui+github@gmail.com>
2015-06-19 19:08:43 +0200

    Adds style to site and adjusted it.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,42 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
+//= require bootstrap.min
 //= require bootstrap-sprockets
 //= require_tree .
 //= require turbolinks
 //= require underscore-min
 //= require gmaps/google
+//= require easing
+//= require jquery.magnific-popup
+//= require move-top
+//= require owl.carousel
+//= require responsiveslides.min
+
+
+// jQuery (necessary for Bootstrap's JavaScript plugins)
+// start-smoth-scrolling
+$(document).ready(function($) {
+  $(".scroll").click(function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+  });
+});
+
+// start-top-nav-script
+$(window).load(function() {
+  var pull 		= $('#pull');
+    menu 		= $('nav ul');
+    menuHeight	= menu.height();
+  $(pull).on('click', function(e) {
+    e.preventDefault();
+    menu.slideToggle();
+  });
+  $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 320 && menu.is(':hidden')) {
+          menu.removeAttr('style');
+        }
+    });
+});
+// End-top-nav-script
