0ab04b6c2e8c8a0d2899cf97c559aa1c30e9c67e
Zhuo-Fei Hui <zhfhui+github@gmail.com>
2015-06-24 21:36:59 +0200

    Adds video_url field to building model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624111427) do
+ActiveRecord::Schema.define(version: 20150624192957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150624111427) do
     t.string   "zipcode",          default: ""
     t.string   "city",             default: ""
     t.string   "country",          default: ""
+    t.string   "video_url",        default: ""
   end
 
   create_table "literatures", force: :cascade do |t|
