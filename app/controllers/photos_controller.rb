5abee60ce8ad054df17adac20696e16481793d1c
Zhuo-Fei Hui <zhfhui+github@gmail.com>
2015-07-01 13:01:43 +0200

    Fixed missing building's name on photo's show site.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,7 +5,7 @@ class PhotosController < ApplicationController
   # GET /photos
   # GET /photos.json
   def index
-    @photos = Photo.all.order(:id)
+    @photos = Photo.all.order(:index_card_no).order(:photo_no)
   end
 
   # GET /photos/1
