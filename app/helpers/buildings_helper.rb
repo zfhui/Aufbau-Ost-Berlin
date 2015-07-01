40dddf2c311c6464917b86e1df1399e7055f50fd
Zhuo-Fei Hui <zhfhui+github@gmail.com>
2015-07-01 20:47:24 +0200

    Gives buildings' external links a better formatting.

--- app/helpers/buildings_helper.rb
+++ app/helpers/buildings_helper.rb
@@ -1,10 +1,37 @@
 module BuildingsHelper
-  def url_html (link, name)
-    if link
-      if !link.empty?
-        content_tag(:a, "#{name}", href: "#{link}", target: "_blank", class: "external_links")
-      end
+  def external_urls_to_str (building)
+    urls = ""
+
+    if building.wiki_url.present?
+      urls << content_tag(:a, "Wikipedia", href: "#{building.wiki_url}", target: "_blank", class: "external_urls")
     end
+
+    if building.wiki_common_url.present?
+      urls << ', ' unless urls.empty?
+      urls << content_tag(:a, "Wikipedia Common", href: "#{building.wiki_common_url}", target: "_blank", class: "external_urls")
+    end
+
+    if building.grossstadt_url.present?
+      urls << ', ' unless urls.empty?
+      urls << content_tag(:a, "Großstadtgeschichten", href: "#{building.grossstadt_url}", target: "_blank", class: "external_urls")
+    end
+
+    if building.denkmalliste_url.present?
+      urls << ', ' unless urls.empty?
+      urls << content_tag(:a, "Denkmalliste", href: "#{building.denkmalliste_url}", target: "_blank", class: "external_urls")
+    end
+
+    if building.other_url.present?
+      urls << ', ' unless urls.empty?
+      urls << content_tag(:a, "Link 1", href: "#{building.other_url}", target: "_blank", class: "external_urls")
+    end
+
+    if building.other_url_2.present?
+      urls << ', ' unless urls.empty?
+      urls << content_tag(:a, "Link 2", href: "#{building.other_url_2}", target: "_blank", class: "external_urls")
+    end
+
+    return urls.html_safe
   end
 
   def next_building(id)
