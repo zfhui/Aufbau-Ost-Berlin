5d5b8b9f32afc286fe17c94f29304c0238f42707
Zhuo-Fei Hui <fei@posteo.de>
2019-11-16 15:43:09 +0100

    Remove 'Quelle'

--- app/controllers/literatures_controller.rb
+++
@@ -1,5 +0,0 @@
-class LiteraturesController < ApplicationController
-  def index
-    @literature = Contentful::Literature.first
-  end
-end
--- app/models/contentful/book.rb
+++
@@ -1,9 +0,0 @@
-module Contentful
-  class Book < ContentfulModel::Base
-    self.content_type_id = 'book'
-
-    belongs_to_many :literatures,
-                    class_name: 'Contentful::Literature',
-                    inverse_of: :books
-  end
-end
